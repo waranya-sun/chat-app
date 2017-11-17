6391d7358e8365214389c08ebb92765087dcf127
Waranya Sunthrakhom <waranya.sun@gmail.com>
2017-11-17 23:12:15 +1100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: FirebaseListObservable<any[]>;
+  msg: string = '';
+  editMsg:boolean = false;
+  editId:number;
+
+  constructor(public af: AngularFireDatabase){
+    this.items = af.list('/messages');
+  }
+
+  send(chatMsg:string){
+    this.items.push({message:chatMsg});
+    this.msg = '';
+  }
+
+  delete(key:string){
+    this.items.remove(key);
+  }
+
+  edit(key:string, message:string){
+    this.items.update(key, {message:message});
+    this.editMsg = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTCf2kNZC_FsvgAIxmG-UdljaGK584LIk",
+  authDomain: "chatapp-3d651.firebaseapp.com",
+  databaseURL: "https://chatapp-3d651.firebaseio.com",
+  projectId: "chatapp-3d651",
+  storageBucket: "",
+  messagingSenderId: "1038652865094"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
